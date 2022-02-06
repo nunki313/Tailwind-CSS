1090a16aa4dcb410b5a0188d8a3439e1e99b362d
nunki313 <nunkiulil@gmail.com>
2022-02-06 17:42:05 +0700

    Tailwind-css
    tugas minggu ke 1

---
+++ talwind-css/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        hijau: "#66CC99",
+        kuning: "#fcd34d",
+        merah: "#ef4444",
+        abu: "#737373",
+        hitam: "#171717",
+        putih: "#f7f7f7",
+      },
+
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      },
+
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1123px",
+        },
+      },
+
+    },
+  },
+  plugins: [],
+}
